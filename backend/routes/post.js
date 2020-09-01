a1a0fcb97a49bc6486f3096817005deb56ce6982
spdeux <sepideh_masoomi@yahoo.com>
2020-09-01 18:30:10 -0400

    adding file middleware

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,48 +1,15 @@
 const express = require("express");
-const multer = require("multer");
 
-const Post = require("../models/post");
 const checkAuth = require("../middleware/check-auth");
-const postController = require("../controllers/post")
+const extractFile = require("../middleware/file");
+const postController = require("../controllers/post");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-    "image/png": "png",
-    "image/jpeg": "jpg",
-    "image/jpg": "jpg",
-};
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const isValid = MIME_TYPE_MAP[file.mimetype];
-        let error = new Error("invalid mime type");
-        if (isValid) {
-            error = null;
-        }
-        cb(error, "backend/images");
-    },
-    filename: (req, file, cb) => {
-        const name = file.originalname.toLowerCase().split(" ").join("-");
-        const ext = MIME_TYPE_MAP[file.mimetype];
-        cb(null, name + "-" + Date.now() + "." + ext);
-    },
-});
-
 //add multer middleware with storage config to extract single file from the incoming request
-router.post(
-    "",
-    checkAuth,
-    multer({ storage: storage }).single("image"),
-    postController.createPost
-);
+router.post("", checkAuth, extractFile, postController.createPost);
 
-router.put(
-    "/:id",
-    checkAuth,
-    multer({ storage: storage }).single("image"),
-    postController.updatePost
-);
+router.put("/:id", checkAuth, extractFile, postController.updatePost);
 
 router.get("", postController.getPosts);
 

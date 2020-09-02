b8876fadd9408eed8f335e034708ff4c9de0e75a
spdeux <sepideh_masoomi@yahoo.com>
2020-09-02 10:28:11 -0400

    adding global configuration.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { AuthData } from "./auth-data.model";
 import { Subject } from "rxjs";
 import { Router } from "@angular/router";
+
+import { environment } from "../../environments/environment";
+import { AuthData } from "./auth-data.model";
+
+const BACKEND_URL = `${environment.apiUrl}/user`;
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
@@ -32,7 +36,7 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post("http://localhost:3000/api/user/signup", authData).subscribe(
+    this.http.post(`${BACKEND_URL}/signup`, authData).subscribe(
       (response) => {
         console.log(response);
         this.router.navigate(["/"]);
@@ -47,7 +51,7 @@ export class AuthService {
     const authData: AuthData = { email: email, password: password };
     this.http
       .post<{ token: string; expiresIn: number; userId: string }>(
-        "http://localhost:3000/api/user/login",
+        `${BACKEND_URL}/login`,
         authData
       )
       .subscribe(
